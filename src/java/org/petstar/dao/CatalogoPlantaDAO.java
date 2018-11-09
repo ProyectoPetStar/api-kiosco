85f6d961624b22daacfe380bb8d6b905b0f4979a
Ramiro Luna <rlunaaragon@gmail.com>
2018-11-09 10:45:53 -0600

    Creación del metodo validaNombrePlanta

--- src/java/org/petstar/dao/CatalogoPlantaDAO.java
+++ src/java/org/petstar/dao/CatalogoPlantaDAO.java
@@ -5,10 +5,28 @@
  */
 package org.petstar.dao;
 
+import javax.sql.DataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.petstar.configurations.PoolDataSource;
+import org.petstar.dto.ResultInteger;
+
 /**
  *
  * @author Ramiro
  */
 public class CatalogoPlantaDAO {
-    
+    public ResultInteger validaNombrePlanta(String nombre_planta) throws Exception{
+        DataSource ds = PoolDataSource.getDataSource();
+        QueryRunner qr = new QueryRunner(ds);
+        StringBuilder sql = new StringBuilder();
+        
+        sql.append("EXEC sp_insertValidaNombrePlanta ?");
+        Object[] params = {nombre_planta};
+        
+        ResultSetHandler rsh = new BeanHandler(ResultInteger.class);
+        ResultInteger count = (ResultInteger) qr.query(sql.toString(), rsh, params);
+        return count;
+    }
 }
