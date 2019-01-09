9f36b75436c4ee2e0820afa641771b030184d7e1
Ramiro Luna <rlunaaragon@gmail.com>
2019-01-08 18:05:20 -0600

    Se agregó el metodo para recuperar contraseña por email

---
+++ src/java/org/petstar/dto/CorreoDTO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.petstar.dto;
+
+/**
+ *
+ * @author Ramiro
+ */
+public class CorreoDTO {
+    String usuarioCorreo;
+    String contrasenia;
+    String destino;
+    String asunto;
+    String mensaje;
+
+    public String getUsuarioCorreo() {
+        return usuarioCorreo;
+    }
+
+    public void setUsuarioCorreo(String usuarioCorreo) {
+        this.usuarioCorreo = usuarioCorreo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }   
+}
